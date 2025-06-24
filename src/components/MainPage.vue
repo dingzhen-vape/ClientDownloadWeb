376df4f80c73e7dbeb09a27b9600c23cf3fed4b5
dingzhen-vape <327112257@qq.com>
2025-06-24 18:10:48 +0800

    Initial commit Changes to be committed: 	new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/MeteorIcon.png 	new file:   src/assets/Tool.css 	new file:   src/assets/Wurst.png 	new file:   src/assets/base.css 	new file:   src/assets/main.css 	new file:   src/components/MainPage.vue 	new file:   src/components/Meteor.vue 	new file:   src/components/WurstCN.vue 	new file:   src/main.js 	new file:   vite.config.js

---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+<template>
+    <!-- 中间 -->
+    <div class="main">
+        <div class="Meteor">
+
+        </div>
+        <div class="Refresh"></div>
+        <div class="WurstCN"></div>
+    </div>
+</template>
