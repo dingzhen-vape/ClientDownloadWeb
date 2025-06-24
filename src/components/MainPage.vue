214c14f44d6fa1bcbce8712ee650f6ddb991fa92
dingzhen-vape <327112257@qq.com>
2025-06-24 18:18:26 +0800

    test

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,7 +5,7 @@
     <!-- 中间 -->
     <div class="main">
         <div class="Meteor">
-
+            <img src="../assets/MeteorIcon.png" alt="">
         </div>
         <div class="Refresh"></div>
         <div class="WurstCN"></div>
